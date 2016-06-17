8c0715fd60595f339e7f38bc0d72df3906708caa
Chris Olstrom <chris@olstrom.com>
2016-06-16 19:58:38 -0700

    Include silence in wrapper

--- lib/instacli.rb
+++ lib/instacli.rb
@@ -3,6 +3,7 @@ require_relative 'instacli/cli'
 require_relative 'instacli/exception_variant'
 require_relative 'instacli/help'
 require_relative 'instacli/demuxing'
+require_relative 'instacli/silence'
 
 # Convenience Wrapper
 module InstaCLI
